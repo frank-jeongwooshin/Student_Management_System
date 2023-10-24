4eb177e5ae0931bc2711df55936582cc87d28e06
frank-jeongwooshin <maecsan12@gmail.com>
2023-10-24 13:20:25 +0900

    test

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,48 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+/*
+Student Management System
+- This project is for managing students simply
+ */
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+import java.util.Scanner;
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+public class Main
+{
+    public static void main(String[] args)
+    {
+        boolean quitFlag = true;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Welcome to SMS !");
+
+        while(quitFlag)
+        {
+            System.out.println("********** MENU **********");
+            System.out.println("1. Register Student.");
+            System.out.println("2. Check Student Information.");
+            System.out.println("3. Edit Student Information.");
+            System.out.println("4. Show All Students.");
+            System.out.println("5. Quit the Program.");
+
+            System.out.print("Please Select Number : ");
+            int inputNum = input.nextInt();
+            System.out.println("**************************");
+
+            switch (inputNum)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    quitFlag = false;
+                    input.close();
+                    break;
+            }
         }
+
     }
 }
